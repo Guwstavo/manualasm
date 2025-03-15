0e6fafaa65c2c0f92028bf97fd7e4bd2efda3206
Guwstavo <gustavoantt2405@gmail.com>
2025-03-14 20:56:31 -0600

    Primer Rep

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+//PANTALLA PRINCIPAL
+import React from 'react'
+import './App.css'
+import Prueba from './components/prueba'
+
+function App() {
+
+  return (
+    <div>
+    <Prueba></Prueba>
+
+    <p></p>
+    </div>
+  )
+}
+
+export default App
