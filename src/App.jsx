31e607413edef62978814605c440d3c3e31ebe7b
Guwstavo <gustavoantt2405@gmail.com>
2025-03-27 01:49:52 -0600

    Manual con GITHUB PAGES

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import React,  {useEffect}  from 'react'
-import { BrowserRouter as Router, Routes, Route,  useLocation } from 'react-router-dom';
-import './App.css'
-import Home from './pages/Homee' 
-import ExampleDetail from './components/ExampleDetail'
+import React, { useEffect } from 'react';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import './App.css';
+import Home from './Homee';
+import ExampleDetail from './components/ExampleDetail';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0); 
-  }, [pathname]); 
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
-  return null; 
+  return null;
 }
 
 function App() {
   return (
     <Router>
       <div className="app-container">
-      <ScrollToTop />
+        <ScrollToTop />
         <main className="main-content">
           <Routes>
             <Route path="/" element={<Home />} />
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/manualasm/', // 👈 esto es CLAVE
   plugins: [react()],
 })
